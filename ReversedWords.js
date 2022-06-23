e38d82506eb06e5afeb1460e38e066dbe7ede38a
Ronald Li <Ronaldnyc88@gmail.com>
2022-06-23 10:15:45 -0400

    a sol

---
+++ ReversedWords.js
@@ -0,0 +1,7 @@
+Example(Input --> Output):
+
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+function reverseWords(str){
+    return str.split(' ').reverse().join(' ')
+  }
