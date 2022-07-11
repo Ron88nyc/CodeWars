8fcfb29e74f4f9c77104e1e654cf492da4fd21ce
Ronald Li <Ronaldnyc88@gmail.com>
2022-07-11 10:34:58 -0400

    sortArr

---
+++ Kyu7/SortArr.js
@@ -0,0 +1,11 @@
+Sort arrays - 1
+
+
+
+
+DESCRIPTION:
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order
+
+sortme = function( names ){
+  return names.sort()
+}
