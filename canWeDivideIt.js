96beb7b58525a9463f42faaf921cb308ca5cc03b
Ronald Li <Ronaldnyc88@gmail.com>
2022-07-18 11:07:58 -0400

    bonusSolution

--- canWeDivideIt.js
+++ canWeDivideIt.js
@@ -1,4 +1,8 @@
+
 Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.
+
+A few cases:
+
 
 (-12, 2, -6)  ->  true
 (-12, 2, -5)  ->  false
@@ -11,6 +15,13 @@ Your task is to create the functionisDivideBy (or is_divide_by) to check if an i
 
 
 
+
+
+
+
+
+
+
 function isDivideBy(number, a, b) {
     if (number % a === 0 && number % b === 0) {
       return true
@@ -18,3 +29,7 @@ function isDivideBy(number, a, b) {
       return false
     }
   }
+
+
+
+  const isDivideBy = (number, a, b) => number % a === 0 && number % b === 0;
