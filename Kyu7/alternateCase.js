16ec64f1eedb17b61065259193bd4ce2b5f41eec
Ronald Li <Ronaldnyc88@gmail.com>
2022-08-12 13:56:33 -0400

    alterSolu

---
+++ Kyu7/alternateCase.js
@@ -0,0 +1,21 @@
+Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+The input will be a lowercase string with no spaces.
+
+Good luck!
+
+If you like this Kata, please try:
+
+Indexed capitalization
+
+Even-odd disparity
+
+
+
+function capitalize(s){
+    const odd = s.split("").map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join("");
+    const even = s.split("").map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join("");
+    return [even, odd];
+  };
