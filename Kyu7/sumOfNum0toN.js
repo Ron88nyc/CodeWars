4990607beb7bbc938e6e6d0d61f4eae2f1aa1abc
Ronald Li <Ronaldnyc88@gmail.com>
2022-08-13 10:49:20 -0400

    EvenSumAR

--- Kyu7/sumOfNum0toN.js
+++ Kyu7/sumOfNum0toN.js
@@ -61,3 +61,6 @@ var SequenceSum = (function() {
 //         : `${Array.from({length: n+1}, (v, k) => k).join('+')}${n?' = ':'='}${n*(n+1)/2}`;
 //     }
 //   }
+
+
+const evenNumbers = (array, number) => array.filter(item => item % 2 === 0).slice(-number);
