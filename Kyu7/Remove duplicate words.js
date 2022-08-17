e03892aefa105f4dd430b1472cb2ba85883fddc9
Ronald Li <Ronaldnyc88@gmail.com>
2022-08-17 15:31:34 -0400

    dupeWords

--- Kyu7/Remove duplicate words.js
+++ Kyu7/Remove duplicate words.js
@@ -33,3 +33,14 @@ function removeDuplicateWords (s) {
       return s.indexOf(n) === i;
     }).join(' ');
   }
+
+  function removeDuplicateWords (s) {
+    // your perfect code...
+    return s.split(' ').filter((ele,i,s) => {
+      return s.indexOf(ele) === i
+    }).join(' ')
+  }
+  
+  // returns a string with no dupes in it
+  // i would first split them into an array to apply the filter method. Next I would use the indexOf to see if the element
+  // is a duplicate of the next iteration. If it is it'll be filtered out, After i'll join it to change it back to a string.
