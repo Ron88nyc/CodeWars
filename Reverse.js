0226636b203a0a70c8472ea56b40063d58c2d2e7
Ronald Li <Ronaldnyc88@gmail.com>
2022-10-05 17:28:56 -0400

    rev solu

---
+++ Reverse.js
@@ -0,0 +1,8 @@
+"   The white space at the beginning of this string doesn't match the whitespace at the end. "
+// reverse it
+" .dne eht ta ecapsetihw eht hctam t'nseod gnirts siht fo gninnigeb eht ta ecaps etihw ehT   "
+
+
+function rev(str) {
+    return rev.reverse(str)
+}
