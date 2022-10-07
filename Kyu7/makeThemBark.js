86dc2fb589e407b5467505c54dc4c1d4b5e13fe9
Ronald Li <Ronaldnyc88@gmail.com>
2022-10-07 11:53:51 -0400

    BarkSolu

--- Kyu7/makeThemBark.js
+++ Kyu7/makeThemBark.js
@@ -1,19 +1,19 @@
-You have been hired by a dogbreeder to write a program to keep record of his dogs.
+// You have been hired by a dogbreeder to write a program to keep record of his dogs.
 
-You've already made a constructor Dog, so no one has to hardcode every puppy.
+// You've already made a constructor Dog, so no one has to hardcode every puppy.
 
-The work seems to be done. It's high time to collect the payment.
+// The work seems to be done. It's high time to collect the payment.
 
-..hold on! The dogbreeder says he wont pay you, until he can make every dog object .bark(). Even the ones already done with your constructor. "Every dog barks" he says. He also refuses to rewrite them, lazy as he is.
+// ..hold on! The dogbreeder says he wont pay you, until he can make every dog object .bark(). Even the ones already done with your constructor. "Every dog barks" he says. He also refuses to rewrite them, lazy as he is.
 
-You can't even count how much objects that bastard client of yours already made. He has a lot of dogs, and none of them can .bark().
+// You can't even count how much objects that bastard client of yours already made. He has a lot of dogs, and none of them can .bark().
 
-Can you solve this problem, or will you let this client outsmart you for good?
+// Can you solve this problem, or will you let this client outsmart you for good?
 
-Practical info:
-The .bark() method of a dog should return the string 'Woof!'.
+// Practical info:
+// The .bark() method of a dog should return the string 'Woof!'.
 
-The contructor you made (it is preloaded) looks like this:
+// The contructor you made (it is preloaded) looks like this:
 
 function Dog(name, breed, sex, age){
     this.name  = name;
@@ -21,3 +21,6 @@ function Dog(name, breed, sex, age){
     this.sex   = sex;
     this.age   = age;
 }
+
+
+Dog.prototype.bark = function() { return 'Woof!' }
