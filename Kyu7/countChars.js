5ac7ba51f54ee7bf0267bd1dbd00c44d041ce2c7
Ronald Li <Ronaldnyc88@gmail.com>
2022-12-07 11:50:23 -0500

    Absent and CountChars Solution

---
+++ Kyu7/countChars.js
@@ -0,0 +1,33 @@
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+
+
+
+function count (string) {  
+    // The function code should be here
+    let countObj = {}
+    for (const i of string) {
+      countObj[i] ? countObj[i]++ : countObj[i] = 1
+    }
+    
+    return countObj
+  }
+
+
+
+
+
+
+
+// function count (string) {  
+//   var count = {};
+//   string.split('').forEach(function(s) {
+//      count[s] ? count[s]++ : count[s] = 1;
+//   });
+//   return count;
+// }
+
+
+
