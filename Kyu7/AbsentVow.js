44e0fa92e2a820075f11eb46102be8024fa922c2
Ronald Li <Ronaldnyc88@gmail.com>
2023-02-21 17:45:45 -0500

    AbsentVowDisplay

---
+++ Kyu7/AbsentVow.js
@@ -0,0 +1,21 @@
+
+Absent vowel
+531592% of 7015,041 of 5,932WesleyYoung2 Issues Reported
+ JavaScript
+Node v18.x
+VIM
+EMACS
+Instructions
+Output
+Your job is to figure out the index of which vowel is missing from a given string:
+
+A has an index of 0,
+E has an index of 1,
+I has an index of 2,
+O has an index of 3,
+U has an index of 4.
+Notes: There is no need for string validation and every sentence given will contain all vowels but one. Also, you won't need to worry about capitals.
+
+Examples
+"John Doe hs seven red pples under his bsket"          =>  0  ; missing: "a"
+"Bb Smith sent us six neatly arranged range bicycles"  =>  3  ; missing: "o"\\
