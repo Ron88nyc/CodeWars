e3446f491d9e1e570c82e55194f35dfaf5c485a8
Ronald Li <Ronaldnyc88@gmail.com>
2023-04-03 14:07:53 -0400

    review

--- Sum of positive.js
+++ Sum of positive.js
@@ -23,6 +23,6 @@ function positiveSum(arr) {
 
 
 
-function positiveSum(arr) {
-   return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
-}
+// function positiveSum(arr) {
+//    return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+// }
